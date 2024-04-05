aab93717e8850bb3e6748ab29264a27c7bfdf303
titibeloha <manrason2@gmail.com>
2024-04-05 22:14:09 +0200

    spring initializer

---
+++ src/main/java/com/test/technique/mowitnow/mowitnow/MowitnowApplication.java
@@ -0,0 +1,13 @@
+package com.test.technique.mowitnow.mowitnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MowitnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MowitnowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/technique/mowitnow/mowitnow/MowitnowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.technique.mowitnow.mowitnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MowitnowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
